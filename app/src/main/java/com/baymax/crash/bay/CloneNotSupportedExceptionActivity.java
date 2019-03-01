94f7a9b22259621679eae65ce8210a17475436d7
liurutong <928825621@qq.com>
2019-03-01 17:04:32 +0800

    提交复现的Crash数据

--- app/src/main/java/com/baymax/crash/bay/CloneNotSupportedExceptionActivity.java
+++ app/src/main/java/com/baymax/crash/bay/CloneNotSupportedExceptionActivity.java
@@ -2,13 +2,24 @@ package com.baymax.crash.bay;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.baymax.crash.R;
 
-public class CloneNotSupportedExceptionActivity extends AppCompatActivity {
+public class CloneNotSupportedExceptionActivity extends AppCompatActivity implements Cloneable{
 
     private TextView exceptionText;
+
+    public CloneNotSupportedExceptionActivity cloned() {
+        try {
+            return (CloneNotSupportedExceptionActivity) super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+
+        }
+        return null;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +32,27 @@ public class CloneNotSupportedExceptionActivity extends AppCompatActivity {
     }
 
     private void initCrash() {
-
+        DemoThread t = new DemoThread();
+        t.cloned();
+        exceptionText.setText(t.getStackTraceString());
     }
 
+}
+
+class DemoThread extends Thread implements Cloneable {
+    private String stackTraceString;
+
+    public DemoThread cloned() {
+        try {
+            return (DemoThread) super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+            stackTraceString = Log.getStackTraceString(e.fillInStackTrace());
+        }
+        return null;
+    }
+
+    public String getStackTraceString() {
+        return stackTraceString;
+    }
 }
--- app/src/main/java/com/baymax/crash/bay/NoSuchMethodExceptionActivity.java
+++ app/src/main/java/com/baymax/crash/bay/NoSuchMethodExceptionActivity.java
@@ -2,9 +2,13 @@ package com.baymax.crash.bay;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.baymax.crash.R;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class NoSuchMethodExceptionActivity extends AppCompatActivity {
 
@@ -16,12 +20,31 @@ public class NoSuchMethodExceptionActivity extends AppCompatActivity {
         setContentView(R.layout.activity_text);
 
         exceptionText = findViewById(R.id.exceptionText);
-
         initCrash();
     }
 
     private void initCrash() {
+        try {
+            executeMethod(Math.class, "max", new Integer[]{1, 2}, // Cannot be int array, but we have auto unboxing.
+                    new Class[]{Integer.class, Integer.class});
 
+//            Math.max对int，long，float和double有4个重载。所有这些都是原始类型。例如，如果要获取int重载，则需要使用int.class而不是Integer.class，否则会得到NoSuchMethodException，因为签名不匹配。
+//            executeMethod(Math.class, "max", new Integer[]{1, 2}, // Cannot be int array, but we have auto unboxing.
+//                    new Class[]{int.class, int.class});
+        } catch (Exception e1) {
+            exceptionText.setText(Log.getStackTraceString(e1.fillInStackTrace()));
+            e1.printStackTrace();
+        }
+    }
+
+    private void executeMethod(Class cls, String methodName, Object[] params, Class[] paramTypes) throws NoSuchMethodException {
+        Method m = cls.getDeclaredMethod(methodName, paramTypes);
+        try {
+            m.invoke(null, params);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            exceptionText.setText(Log.getStackTraceString(e.fillInStackTrace()));
+            e.printStackTrace();
+        }
     }
 
 }
