fcf8b9b1bb54cdc39d15b71f0a6a6a98076346e8
liurutong <928825621@qq.com>
2019-03-04 19:25:53 +0800

    线程Crash

--- app/src/main/java/com/baymax/crash/bay/InterruptedExceptionActivity.java
+++ app/src/main/java/com/baymax/crash/bay/InterruptedExceptionActivity.java
@@ -2,6 +2,7 @@ package com.baymax.crash.bay;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.baymax.crash.R;
@@ -23,28 +24,37 @@ public class InterruptedExceptionActivity extends AppCompatActivity {
     }
 
     private void initCrash() {
+        Thread thread = new Thread(){
+            public void run(){
+                System.out.println(Thread.currentThread().getName()+ "线程开始了~");
+                try {
+                    sleep(1000);
+                } catch (InterruptedException e) {
+                    exceptionText.setText(Log.getStackTraceString(e.fillInStackTrace()));
+                    System.out.println(Thread.currentThread().getName()+"抛出了InterruptedException");
+                    System.out.println("2" + isInterrupted());
+                }
+                System.out.println(Thread.currentThread().getName()+ "线程结束了~");
+            }
+        };
+        thread.start();
+        System.out.println("1" + thread.isInterrupted());
+        thread.interrupt();
 
+        System.out.println("测试结束");
     }
 
 }
 
-//class TaskRunner implements Runnable {
-//    private BlockingQueue<Task> queue;
-//
-//    public TaskRunner(BlockingQueue<Task> queue) {
-//        this.queue = queue;
-//    }
-//
-//    public void run() {
-//        try {
-//            while (true) {
-//                Task task = queue.take(10, TimeUnit.SECONDS);
-//                task.execute();
-//            }
-//        }
-//        catch (InterruptedException e) {
-//            // Restore the interrupted status
-//            Thread.currentThread().interrupt();
-//        }
-//    }
-//}
+/**
+ * 没有阻塞操作的线程
+ */
+class NonBlockedRunnable implements Runnable {
+    @Override
+    public void run() {
+        while (!Thread.interrupted()) {
+            System.out.println("线程执行中...");
+        }
+    }
+
+}
