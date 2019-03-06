3c112ed970677f868cdae40e2720f89ae0064c0d
cugyong <455399810@qq.com>
2019-03-06 16:35:20 +0800

    3/6 夏勇 crash复现

--- app/src/main/java/com/baymax/crash/bay/TimeoutExceptionActivity.java
+++ app/src/main/java/com/baymax/crash/bay/TimeoutExceptionActivity.java
@@ -6,6 +6,12 @@ import android.widget.TextView;
 
 import com.baymax.crash.R;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+
 public class TimeoutExceptionActivity extends AppCompatActivity {
 
     private TextView exceptionText;
@@ -21,7 +27,29 @@ public class TimeoutExceptionActivity extends AppCompatActivity {
     }
 
     private void initCrash() {
+        ExecutorService executor = Executors.newCachedThreadPool();
+        Task task = new Task();
+        FutureTask<Integer> futureTask = new FutureTask<Integer>(task);
+        executor.submit(futureTask);
+        executor.shutdown();
+        try {
+            futureTask.get(1, TimeUnit.MILLISECONDS);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 
+
+    class Task implements Callable<Integer> {
+        @Override
+        public Integer call() throws Exception {
+            System.out.println("子线程在进行计算");
+            Thread.sleep(3000);
+            int sum = 0;
+            for(int i=0;i<100;i++)
+                sum += i;
+            return sum;
+        }
     }
 
 }
